b607d02bf15e1bbc3cd6020840998b4cc39f9841
Stefan Karlsson <phtefan.karlsson@gmail.com>
2023-04-27 19:46:13 +0200

    Add Consumer.java

---
+++ consumer/src/main/java/org/example/consumer/Consumer.java
@@ -0,0 +1,10 @@
+package org.example.consumer;
+
+import org.example.service.CurrencyExchange;
+public class Consumer {
+    public static void main(String[] args) {
+
+        CurrencyExchange currencyExchange;
+
+    }
+}
